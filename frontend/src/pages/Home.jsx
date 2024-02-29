404f6913bb48950f02a0acebc8091aff9a8cc48f
Divyansh Srivastavae <divyanshsrivastava215@gmail.com>
2024-03-01 00:20:31 +0530

    Doctors Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,6 +12,7 @@ import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
+import DoctorList from "../components/Doctors/DoctorList";
 
 const Home = () => {
   return (
@@ -251,8 +252,25 @@ const Home = () => {
           </div>
         </div>
       </section>
-
       {/* =========== feature section end ================= */}
+
+      {/* =========== our great doctors section end ================= */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Great Doctors</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care
+            </p>
+          </div>
+
+          <DoctorList/>
+
+        </div>
+      </section>
+
+      {/* =========== our great doctors section end ================= */}
     </>
   );
 };
